10b4510dcf6152d9877b7e5004af11c307305397
Escape96X <jiricek.milan01@gmail.com>
2020-12-15 19:10:57 +0100

    Redukce board

--- Code/ESP8266_ON/web.h
+++ Code/ESP8266_ON/web.h
@@ -4,9 +4,6 @@
 #include <Arduino.h>
 #include <ESP8266WebServer.h>
 
-//extern ESP8266WebServer server(80);
-//bool APWork;
-
 void setWiFiServer();
 String readEEPROM(int numberOfStart, int len);
 extern String ssid;
